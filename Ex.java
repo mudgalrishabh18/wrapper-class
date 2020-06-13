47ef484425c695d4bb9081afe3ed13fcd5c8b084
Rishabh Mudgal <62346513+mudgalrishabh18@users.noreply.github.com>
2020-06-13 09:49:22 +0530

    Add files via upload

---
+++ Ex.java
@@ -0,0 +1,21 @@
+package wrapperclass;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Integer myInt = 5;
+		    Double myDouble = 5.99;
+		    Character myChar = 'A';
+		    System.out.println(myInt);
+		    System.out.println(myDouble);
+		    System.out.println(myChar);
+		    System.out.println(myInt.intValue());
+		    System.out.println(myDouble.doubleValue());
+		    System.out.println(myChar.charValue());
+		    String myString = myInt.toString();
+		    System.out.println(myString.length());
+
+	}
+
+}
